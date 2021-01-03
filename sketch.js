faba2719b9699e68286a008d439c79171ffafc43
hiIExistMan <71188354+hiIExistMan@users.noreply.github.com>
2021-01-02 23:29:21 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ const dragger = document.querySelector("a");
 const code = document.querySelector("textarea");
 const name = document.querySelector("input");
 code.addEventListener("keyup", () => {
-  dragger.href = `javascript:(function(){${code}})();`;
+  dragger.href = `javascript:(function(){${code.value}})();`;
   dragger.innerHTML = name.value;
 });
 
