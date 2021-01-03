a0133151ea2791015516d48411edd74bb68dd057
hiIExistMan <71188354+hiIExistMan@users.noreply.github.com>
2021-01-02 22:45:42 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+const dragger = document.querySelector("a");
+const code = document.querySelector("textarea");
+const name = document.querySelector("input");
+code.addEventListener("keyup", () => {
+  dragger.href = `javascript:(function(){${code}})();`;
+  dragger.innerHTML = name.value;
+});
+
+name.addEventListener("keyup", () => {
+    dragger.innerHTML = name.value;
+});
